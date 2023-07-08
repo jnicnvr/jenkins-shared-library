98891f3c6aa6118d21b72d463104998d6cc61f2a
Konics.dev <konics.dev@gmail.com>
2023-07-08 14:31:25 +0800

    test

---
+++ vars/jenkins.groovy
@@ -0,0 +1,6 @@
+ 
+ def setBuildDescriptionForCiPipeline(branch,commitId,commitAuthor,commitMessage){
+    def description = "<br> <b>Branch: </b> ${branch}<br> <b>Commit ID:</b> ${commitId} <br> <b>Author:</b> ${commitAuthor} <br> <b> CommitMessage: </b> ${commitMessage} <br>"
+     currentBuild.setDescription(description)
+ }
+ 
