f3a12eb7d02f3915d6ab913bff8ad825fc4d85f4
Konics.dev <konics.dev@gmail.com>
2023-07-13 11:36:27 +0800

    test

--- vars/ddocker.groovy
+++ vars/ddocker.groovy
@@ -2,8 +2,16 @@
 def build(imageTag,branchName,author,commitId){
     echo "${imageTag}-${branchName}-${author}-${commitId}"
     docker.build(imageTag)
-
+    return G_CI_VAR['isImagePush']
     // "--build-arg BRANCH_NAME=${branchName} --build-arg AUTHOR=${author} --build-arg COMMIT_ID=${commitId} ."
+}
+
+def push(imageTag,artifactoryName){
+    echo "${imageTag}-${artifactoryName}"
+    
+    // sh "docker login -u konicsdev -p konics.dev"
+    // sh "docker push ${imageTag}"
+    // sh 'docker logout'
 }
 
 def version(){
