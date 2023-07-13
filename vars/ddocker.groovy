83482907fe424b81d953d274e95fea7848c111b6
Konics.dev <konics.dev@gmail.com>
2023-07-13 10:56:06 +0800

    test

--- vars/ddocker.groovy
+++ vars/ddocker.groovy
@@ -1,7 +1,7 @@
 
 def build(imageTag,branchName,author,commitId){
     echo "${imageTag}-${branchName}-${author}-${commitId}"
-    docker.build(imageTag, "--build-arg BRANCH_NAME=${branchName} --build-arg AUTHOR=${author} --build-arg COMMIT_ID=${commitId} .")
+    sh "docker build -t ${imageTag} --build-arg BRANCH_NAME=${branchName} --build-arg AUTHOR=${author} --build-arg COMMIT_ID=${commitId} ."
 }
 
 def version(){
