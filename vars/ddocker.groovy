1a6b70f476097c64d4b27e0090b9ace99582c84a
Konics.dev <konics.dev@gmail.com>
2023-07-13 10:30:53 +0800

    test

--- vars/ddocker.groovy
+++ vars/ddocker.groovy
@@ -8,6 +8,6 @@ def version(){
    sh 'docker version'
 }
 
-def setImageName(String artifactoryName){
-    return "${artifactoryName}"
+def setImageName(String artifactoryName,String image,String branch,String buildNumber){
+    return "${artifactoryName}-${image}-${branch}-${buildNumber}"
 }
