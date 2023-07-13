dd106e6f7a70ff99254ae7f5119099524f754120
Konics.dev <konics.dev@gmail.com>
2023-07-13 10:36:55 +0800

    test

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,5 +1,5 @@
- def setBuildDescriptionForCiPipeline(branch,commitId,commitAuthor,commitMessage){
-    def description = "<br> <b>Branch: </b> ${branch}<br> <b>Commit ID:</b> ${commitId} <br> <b>Author:</b> ${commitAuthor} <br> <b> CommitMessage: </b> ${commitMessage} <br>"
+ def setBuildDescriptionForCiPipeline(branch,commitId,commitAuthor,commitMessage,image){
+    def description = "<br> <b>Branch: </b> ${branch}<br> <b>Commit ID:</b> ${commitId} <br> <b>Author:</b> ${commitAuthor} <br> <b> CommitMessage: </b> ${commitMessage} <br><b> Image: </b> ${image} <br>"
      currentBuild.setDescription(description)
  }
  
